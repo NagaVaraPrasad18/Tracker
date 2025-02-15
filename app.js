b71543abf5efcd91f14a098de7d4b76f7c2ff853
NagaVaraPrasad <nagamallinagavaraprasad@gmail.com>
2025-02-16 00:39:42 +0530

    Fixed Error Message

--- app.js
+++ app.js
@@ -320,6 +320,7 @@ const createHabitCard = (habit) => {
 
   document.addEventListener('click', () => {
     menuContent.classList.add('hidden');
+	document.getElementById("errorMsg").classList.add("hidden");
   });
 
   const deleteBtn = card.querySelector('.delete-btn');
